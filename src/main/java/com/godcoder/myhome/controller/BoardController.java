478dfe91e30590b5a59326151ad30bd234a4afec
MaJunghan <wjdgksak@naver.com>
2020-11-29 20:44:27 +0900

    form 글쓰기

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,7 +21,7 @@ public class BoardController {
     // @Autowired 사용해서 BoardRepository객체를 사용하겠다
 
 
-
+    // db에 있는 테이블을 list로 뿌리는 메서드
     @GetMapping("/list")
     // db값 가져와야하니까 model 생성
     public String list(Model model)  {
@@ -27,4 +29,20 @@ public class BoardController {
       model.addAttribute("boards", boards);
       return "board/list";
     }
+
+    // form.html의 데이터를 모델에담아서 해당모델 Board()를 자기자신에게보냄
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("board", new Board() );
+        return "board/form";
+    }
+
+    //  ModelAttribute 반환받아서 그걸 db에 save 하고 list페이지로가는것.
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board) {
+        // save : key값이 있으면 update 없으면 insert
+        boardRepository.save(board);
+        return "redirect:/board/list";
+    }
+
 }

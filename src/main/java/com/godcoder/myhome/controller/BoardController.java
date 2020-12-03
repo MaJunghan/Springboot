2eb060e96154746321d5a2d71e14df356e8ae08f
MaJunghan <wjdgksak@naver.com>
2020-12-03 19:47:16 +0900

    검색기능

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -29,24 +29,26 @@ public class BoardController {
     // db에 있는 테이블을 list로 뿌리는 메서드
     @GetMapping("/list")
     // db값 가져와야하니까 model 생성
-    public String list(Model model, @PageableDefault(size = 2) Pageable pageable)  {
-      Page<Board> boards = boardRepository.findAll(pageable);
-      int startPage = Math.max(1,boards.getPageable().getPageNumber() - 4);
-      int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
-      model.addAttribute("startPage", startPage);
-      model.addAttribute("endPage", endPage);
-      model.addAttribute("boards", boards);
-      return "board/list";
+    public String list(Model model, @PageableDefault(size = 5) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+        //Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("boards", boards);
+        return "board/list";
     }
 
     // form.html의 데이터를 모델에담아서 해당모델 Board()를 자기자신에게보냄
     @GetMapping("/form")
     // required는 필수값안들어오면 에러창발생 : 이미작성된글을 수정하기떄문에 id가 무조건있음 필수 no
-    public String form(Model model, @RequestParam(required = false) Long id ) {
-        if(id == null) {
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
             // id의 값no => 기존대로 board를넘겨줌
-            model.addAttribute("board", new Board() );
-        } else{
+            model.addAttribute("board", new Board());
+        } else {
             // id의 값ok => db에 해당id가있는지 찾아서 board에 넣어줌.
             Board board = boardRepository.findById(id).orElse(null);
             model.addAttribute("board", board);
@@ -56,7 +58,7 @@ public class BoardController {
 
     //  ModelAttribute 반환받아서 그걸 db에 save 하고 list페이지로가는것.
     @PostMapping("/form")
-    public String greetingSubmit(@Valid Board board,  BindingResult bindingResult) {
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
         boardValidator.validate(board, bindingResult);
         if (bindingResult.hasErrors()) {
             return "board/form";
